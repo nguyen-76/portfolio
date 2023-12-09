7cb2923f3a8b432e0439fed335145ab5133dd7b6
Nguyen Doan <nguyendoan3456@gmail.com>
2023-12-09 07:11:26 -0500

    add in css and add h1 at line 53

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import "./App.css";
 const api = {
     key: "6b5b26bba33aada4aa941d7c12bab231",
     base: "https://api.openweathermap.org/data/2.5",
@@ -18,8 +19,9 @@ useEffect(() => {
       const url = `${api.base}/weather?q=${searchCity}&units=metric&appid=${api.key}`;
       const res = await fetch(url);
       const data = await res.json();
+      console.log(data);
       if (res.ok) {
-        setWeatherInfo(`${data.name}, ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`);
+        setWeatherInfo(`${data.name} ${data.sys.country} ${data.weather[0].description} ${data.main.temp}Celsius`);
         setErrorMes("");
       } else {
         setErrorMes(data.message);
@@ -47,7 +49,10 @@ const handleSumbit = (e) => {
     </form>
     {loading ? (<div>Loading...</div>) : (
       <>
-        {errorMes ? (<div style={{color: "red"}}>{errorMes}</div>) : (<div>{weatherInfo}</div>)}
+        {errorMes ? (<div style={{color: "red"}}>{errorMes}</div>) : (<div className="container">
+          <h1>Today's weather</h1>
+          <div>{weatherInfo}</div>
+          </div>)}
       </>
     )}
 </>
